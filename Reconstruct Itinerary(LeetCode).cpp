65c5100a32065797618b620e199a4385cf05d725
DEO-DECODE <106802468+DEO-DECODE@users.noreply.github.com>
2023-03-28 11:51:01 +0530

    Create Reconstruct Itinerary(LeetCode).cpp

---
+++ Reconstruct Itinerary(LeetCode).cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Reconstruct Itinerary(LeetCode)
+vector<string> ans;
+map<string, multiset<string>> mp;
+void dfs(string src)
+{
+    while (mp[src].size() > 0)
+    {
+        string next = *mp[src].begin();
+        mp[src].erase(mp[src].begin());
+        dfs(next);
+    }
+    ans.push_back(src);
+}
+vector<string> findItinerary(vector<vector<string>> &tickets)
+{
+    for (int i = 0; i < tickets.size(); ++i)
+    {
+        mp[tickets[i][0]].insert(tickets[i][1]);
+    }
+    // mp formed
+    dfs("JFK");
+    // sort(ans.begin(), ans.end());
+    return vector<string>(ans.rbegin(), ans.rend());
+}
